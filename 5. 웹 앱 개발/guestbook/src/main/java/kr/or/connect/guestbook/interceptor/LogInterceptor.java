b855e145762cdbbbeff536dcff3cb308bfbf4652
Yoon-Suji <70956926+Yoon-Suji@users.noreply.github.com>
2021-05-18 00:34:32 +0900

    5-6. 실습 코드 추가

---
+++ 5. 웹 앱 개발/guestbook/src/main/java/kr/or/connect/guestbook/interceptor/LogInterceptor.java
@@ -0,0 +1,24 @@
+package kr.or.connect.guestbook.interceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+public class LogInterceptor extends HandlerInterceptorAdapter {
+	@Override
+	//Controller 메소드가 실행된 후에 실행
+	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+			ModelAndView modelAndView) throws Exception {
+		System.out.println(handler.toString() + " 가 종료되었습니다.  " + modelAndView.getViewName() + "을 view로 사용합니다.");
+	}
+
+	@Override
+	//Controller 메소드가 실행되기 전에 실행
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+		System.out.println(handler.toString() + " 를 호출했습니다.");
+		return true;
+	}
+}
